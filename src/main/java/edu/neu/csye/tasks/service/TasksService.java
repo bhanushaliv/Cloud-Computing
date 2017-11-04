1e77187fd264f493dc5bc52464d36961dc5ce6fe
shrikant <mudholkar.s@husky.neu.edu>
2017-11-03 23:48:49 -0400

    f

--- src/main/java/edu/neu/csye/tasks/service/TasksService.java
+++ src/main/java/edu/neu/csye/tasks/service/TasksService.java
@@ -45,7 +45,7 @@ public class TasksService {
 
     private static String bucketName = "csye6225-fall2017-mudholkars.me.csye6225.com";
     private static Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-    private static String keyName        = "File" + timestamp.toString();
+    private static String keyName = "File" + timestamp.toString();
 
 
     /**
